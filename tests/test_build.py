fa3feac7b4dc153b80e8591e92ef2f6ad0855588
Hynek Schlawack <hs@ox.cx>
2026-06-19 21:13:19 +0200

    Simplify Pyiodite detection (#130)

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,11 +1,6 @@
-import sys
-
 import pytest
 
-from _argon2_cffi_bindings._ffi_build import (
-    _get_target_platform,
-    _is_emscripten_build,
-)
+from _argon2_cffi_bindings._ffi_build import _get_target_platform
 
 
 @pytest.mark.parametrize(
@@ -24,21 +19,3 @@ def test_arch(arch_flags, expected):
     it doesn't find anything.
     """
     assert expected == _get_target_platform(arch_flags, "FOO")
-
-
-def test_is_emscripten_build_from_platform(monkeypatch):
-    """
-    emscripten as a platform is detected.
-    """
-    monkeypatch.setattr(sys, "platform", "emscripten")
-
-    assert _is_emscripten_build()
-
-
-def test_is_emscripten_build_from_pyodide_env(monkeypatch):
-    """
-    emscripten is detected via env var.
-    """
-    monkeypatch.setenv("PYODIDE", "1")
-
-    assert _is_emscripten_build()
